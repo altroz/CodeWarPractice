15020738652bded9879008f5c901824cc836b94c
altroz <nhb913@outlook.com>
2015-06-20 20:17:47 -0700

    Quick Test

---
+++ src/problem1/Multiply.java
@@ -0,0 +1,7 @@
+package problem1;
+
+public class Multiply {
+	public Double multiply(Double a, Double b){
+		return (a*b);
+	}
+}
---
+++ src/problem1/MultiplyTest.java
@@ -0,0 +1,12 @@
+package problem1;
+
+public class MultiplyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Multiply m = new Multiply();
+		double s=m.multiply(5.0, 4.0);
+		System.out.println(s);
+	}
+
+}
