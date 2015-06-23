3af6c639823f0659346fd7e6e54f5089a75ec655
altroz <nhb913@outlook.com>
2015-06-22 23:18:06 -0700

    Managed to solve the heart of the problem. But the implementation is required.

--- src/problem4/Marathon.java
+++ src/problem4/Marathon.java
@@ -2,35 +2,31 @@ package problem4;
 
 public class Marathon {
 
-	public static void Rank() {
-		String[] Names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil",
-				"Matt", "Alex", "Emma", "John", "James", "Jane", "Emily",
-				"Daniel", "Neda", "Aaron", "Kate" };
+	public static int getMinIndex(int[] values) {
+		int minValue = Integer.MAX_VALUE;
+		int minIndex = -1;
 
-		int[] Minutes = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334,
-				412, 393, 299, 343, 317, 265 };
-
-		int PrimaryPlace=Minutes[0];
-		String winner = " ";
-		int SecondaryPlace=Minutes[0];
-		String runnerup = " ";
-		
-		for (int i = 0; i < Minutes.length; i++) {
-		
-			if (PrimaryPlace > Minutes[i]) {
-				PrimaryPlace = Minutes[i];
-				winner = Names[i];
-			} 
-			else if (Minutes[i] != PrimaryPlace && SecondaryPlace > Minutes[i]) {
-				SecondaryPlace = Minutes[i];
-				runnerup = Names[i];
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] < minValue) {
+				minValue = values[i];
+				minIndex = i;
 			}
 		}
-		System.out.println("The Fastest Minute is: " + PrimaryPlace + " min. The Winner is: " + winner);
-		System.out.println("The Second Fastest Minute is: " + SecondaryPlace + " min. The Runnerup is: " + runnerup);
+		return minIndex;
 	}
 
-	public static void main(String[] args) {
-		Rank();
+	public static int getSecondMinIndex(int[] values){
+		int secondIdx = -1;
+		int minIdx = getMinIndex(values);
+		
+		for(int i = 0; i<values.length; i++){
+			if (i == minIdx)
+				continue;
+			if((secondIdx ==-1) || values[i] < values[secondIdx]){
+				secondIdx = i;
+			}
+		}
+		return secondIdx;
 	}
+
 }
