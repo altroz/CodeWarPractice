93485caab3d92bc6756ad083c2d034a168ef417f
altroz <nhb913@outlook.com>
2015-06-26 00:07:06 -0700

    Quick Class Fundalmental commits

---
+++ src/ClassFundalmental/ClassTest.java
@@ -0,0 +1,14 @@
+package ClassFundalmental;
+
+public class ClassTest {
+	int/*Member instance type declaration*/ x, y, radius; /*Member instance*/
+	
+	//Member methods
+	void area(){
+		//method Body
+	}
+	
+	void fillColor(){
+		//method Body
+	}
+}
